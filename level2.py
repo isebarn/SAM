e074ee9b88d734ef87526c5b7d6bb64f9ae135ee
isebarn <isebarn182@gmail.com>
2020-08-13 08:23:00 +0000

    Fix

--- level2.py
+++ level2.py
@@ -4,13 +4,13 @@ import argparse
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  #parser.add_argument("filename", type=str, help="Site file name")
-  #parser.add_argument('cores', nargs='?', default=2, type=int, help="No. cores")
+  parser.add_argument("filename", type=str, help="Site file name")
+  parser.add_argument('cores', nargs='?', default=2, type=int, help="No. cores")
 
   args = parser.parse_args()
 
-  filename = 'sites.txt'#args.filename
-  cores = 3#args.cores
+  filename = args.filename
+  cores = args.cores
   site_file = open(filename, 'r')
   urls = [x.strip() for x in site_file.readlines()]
 
